ef1365b1391e177fd3c6ba04e544b8249b0b5a80
Eugene Zhlobo <ezhlobo@gmail.com>
2017-09-19 13:57:50 +0300

    Add uses-react rule

---
+++ tests/lib/rules/uses-react.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Prevent React to be marked as unused
+ * @author Eugene Zhlobo
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const eslint = require('eslint')
+const ruleNoUnusedVars = require('eslint/lib/rules/no-unused-vars')
+const RuleTester = eslint.RuleTester
+
+const rule = require('../../../lib/rules/uses-react')
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+  },
+}
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions })
+
+const linter = ruleTester.linter || eslint.linter
+linter.defineRule('uses-react', rule)
+
+ruleTester.run('rule "uses-react"', ruleNoUnusedVars, {
+  valid: [
+    {code: '/*eslint uses-react:1*/ var React; pug``;'},
+    {code: '/*eslint uses-react:1*/ var React; (function() { return pug``; })()'},
+    {code: '/*eslint uses-react:1*/ var React; class Component {render() { return pug``; }}; new Component();'},
+  ],
+  invalid: [
+    {
+      code: '/*eslint uses-react:1*/ var React;',
+      errors: [{ message: '\'React\' is defined but never used.' }],
+    },
+  ],
+})
