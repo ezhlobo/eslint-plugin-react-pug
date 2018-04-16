054d13c23c2802a4a739c0f54859ad58f7cac90c
Eugene Zhlobo <ezhlobo@gmail.com>
2018-04-16 14:12:28 +0300

    Improve tests with broken templates: add use cases with objects

--- tests/lib/rules/no-broken-template.js
+++ tests/lib/rules/no-broken-template.js
@@ -27,11 +27,18 @@ ruleTester.run('rule "no-broken-template"', rule, {
   valid: [
     { code: 'pug`p= variable`;' },
     { code: 'pug`Component`;' },
+    {
+      code: `pug\`
+        div
+          Component(
+            object={a: 1, b: 2}
+          )
+      \``,
+    },
   ],
   invalid: [
     {
       code: `
-        /*global pug*/
         pug\`
           p good string
           div
@@ -39,10 +46,54 @@ ruleTester.run('rule "no-broken-template"', rule, {
         \`;
       `,
       errors: [{
-        line: 6,
+        line: 5,
         column: 13,
-        endLine: 6,
+        endLine: 5,
         endColumn: 31,
+        message: 'Pug can\'t parse this template',
+      }],
+    },
+    {
+      code: `
+        pug\`
+          p good string
+          div
+            each i in [1, 2, 3
+        \`;
+      `,
+      errors: [{
+        line: 5,
+        column: 13,
+        endLine: 5,
+        endColumn: 31,
+        message: 'Pug can\'t parse this template',
+      }],
+    },
+    {
+      code: `pug\`
+        Component(
+          iam-object= a: 1, b: 2 }
+        )
+      \`;`,
+      errors: [{
+        line: 3,
+        column: 11,
+        endLine: 3,
+        endColumn: 35,
+        message: 'Pug can\'t parse this template',
+      }],
+    },
+    {
+      code: `pug\`
+        Component(
+          iam-object={ a: 1, b: 2
+        )
+      \`;`,
+      errors: [{
+        line: 4,
+        column: 9,
+        endLine: 4,
+        endColumn: 10,
         message: 'Pug can\'t parse this template',
       }],
     },
