a5f9d0a2d4e11fec2039a4d5450bd14a8837e3d1
Eugene Zhlobo <ezhlobo@gmail.com>
2018-08-22 17:02:26 +0300

    Include quotes rule to main settings

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * @fileoverview Add supporting of pugjs with react
- * @author
  */
 
 /* eslint-disable global-require */
 const allRules = {
   'no-broken-template': require('./lib/rules/no-broken-template'),
   'no-undef': require('./lib/rules/no-undef'),
+  quotes: require('./lib/rules/quotes'),
   'uses-react': require('./lib/rules/uses-react'),
   'uses-vars': require('./lib/rules/uses-vars'),
 }
@@ -22,6 +22,7 @@ module.exports = {
       rules: {
         'react-pug/no-broken-template': 2,
         'react-pug/no-undef': 2,
+        'react-pug/quotes': 2,
         'react-pug/uses-react': 2,
         'react-pug/uses-vars': 2,
       },
