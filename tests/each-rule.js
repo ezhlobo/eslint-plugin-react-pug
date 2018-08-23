535e7b1ff7d2514bc0b5af4899d2aeb6dd12d61b
Eugene Zhlobo <ezhlobo@gmail.com>
2018-08-23 09:14:40 +0300

    Add no-interpolation rule to README and write tests that all rules are mentioned there

--- tests/each-rule.js
+++ tests/each-rule.js
@@ -7,15 +7,36 @@ const { docsUrl } = require('../lib/util/eslint')
 const ruleFiles = fs.readdirSync(path.resolve(__dirname, '..', 'lib', 'rules'))
   .map(file => path.basename(file, '.js'))
 
-describe('URLs to documentation', () => {
-  ruleFiles.forEach((ruleName) => {
-    it(ruleName, () => {
-      const filepath = path.resolve(__dirname, '..', 'lib', 'rules', `${ruleName}.js`)
+describe('each rule', () => {
+  describe('has url to documentation', () => {
+    ruleFiles.forEach((ruleName) => {
+      it(ruleName, () => {
+        const filepath = path.resolve(__dirname, '..', 'lib', 'rules', `${ruleName}.js`)
 
-      // eslint-disable-next-line global-require, import/no-dynamic-require
-      const file = require(filepath)
+        // eslint-disable-next-line global-require, import/no-dynamic-require
+        const file = require(filepath)
 
-      assert.equal(file.meta.docs.url, docsUrl(ruleName))
+        assert.equal(file.meta.docs.url, docsUrl(ruleName))
+      })
+    })
+  })
+
+  describe('mentioned in README', () => {
+    const listOfRulesInReadme = fs
+      .readFileSync(path.resolve(__dirname, '..', 'README.md'), 'utf-8')
+      .match(/## List of supported rules\n+((\*.+\n+)+)/m)[1]
+      .split('\n')
+      .filter(Boolean)
+
+    ruleFiles.forEach((ruleName) => {
+      it(ruleName, () => {
+        const checker = new RegExp(`^\\* \\[\`react-pug/${ruleName}\`\\]\\(\\./docs/rules/${ruleName}.md\\): .+$`)
+
+        const isPresented = listOfRulesInReadme
+          .find(item => checker.test(item))
+
+        assert(isPresented)
+      })
     })
   })
 })
