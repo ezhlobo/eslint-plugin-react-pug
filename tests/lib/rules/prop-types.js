fd611889d51ada36eec2e84160ca3e374c175b4e
Eugene Zhlobo <ezhlobo@gmail.com>
2019-06-15 23:47:32 +0300

    Start reporting about unused variables

--- tests/lib/rules/prop-types.js
+++ tests/lib/rules/prop-types.js
@@ -18,6 +18,9 @@ const rule = require('../../../lib/rules/prop-types')
 const parserOptions = {
   ecmaVersion: 8,
   sourceType: 'module',
+  ecmaFeatures: {
+    experimentalObjectRestSpread: true,
+  },
 }
 
 //------------------------------------------------------------------------------
@@ -26,21 +29,84 @@ const parserOptions = {
 
 const ruleTester = new RuleTester({ parserOptions })
 
+const buildUnusedMessage = name => `'${name}' PropType is defined but prop is never used`
+
 const cases = [
   {
-    name: 'Basic',
+    name: 'Functional Component, Unused props',
     valid: {
       code: `
         function Component(props) {
-          return props.name
+          console.log(props.name)
         }
         Component.propTypes = { name: PropTypes.string }
       `,
     },
     invalid: {
-      code: ``,
-      errors: [],
+      code: `
+        function Component(props) {
+          console.log(props)
+        }
+        Component.propTypes = { name: PropTypes.string, second: PropTypes.bool }
+      `,
+      errors: [
+        buildError([5, 33], [5, 37], buildUnusedMessage('name')),
+        buildError([5, 57], [5, 63], buildUnusedMessage('second')),
+      ],
     },
+  },
+
+  {
+    name: 'Functional Component, Unused props, spreading',
+    valid: [
+      {
+        code: `
+          function Component(props) {
+            const { name, ...rest } = props
+
+            console.log(rest.test)
+          }
+          Component.propTypes = { name: PropTypes.string, test: PropTypes.bool }
+        `,
+      },
+      {
+        code: `
+          function Component(props) {
+            const { ...rest } = props
+            const { ...secondRest } = rest
+
+            console.log(secondRest.name, rest.test)
+          }
+          Component.propTypes = { name: PropTypes.string, test: PropTypes.bool }
+        `,
+      },
+    ],
+    invalid: [
+      {
+        code: `
+          function Component(props) {
+            const { name, ...rest } = props
+          }
+          Component.propTypes = { name: PropTypes.string, test: PropTypes.bool }
+        `,
+        errors: [
+          buildError([5, 59], [5, 63], buildUnusedMessage('test')),
+        ],
+      },
+      {
+        code: `
+          function Component(props) {
+            const { ...rest } = props
+            const { ...secondRest } = rest
+          }
+          Component.propTypes = { name: PropTypes.string, test: PropTypes.bool }
+        `,
+        errors: [
+          buildError([6, 35], [6, 39], buildUnusedMessage('name')),
+          buildError([6, 59], [6, 63], buildUnusedMessage('test')),
+        ],
+      },
+    ],
   },
 ]
 
