c1268d36044a37720e23a7cc5e693c0dcdda7828
Eugene Zhlobo <ezhlobo@gmail.com>
2019-06-15 16:43:47 +0300

    Grab prop-types definitions

---
+++ tests/lib/rules/prop-types.js
@@ -0,0 +1,49 @@
+/**
+ * @fileoverview Tests for uses-react
+ * @author Eugene Zhlobo
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const eslint = require('eslint')
+const buildError = require('../../../lib/util/testBuildError')
+const buildCases = require('../../../lib/util/testBuildCases')
+
+const { RuleTester } = eslint
+
+const rule = require('../../../lib/rules/prop-types')
+
+const parserOptions = {
+  ecmaVersion: 8,
+  sourceType: 'module',
+}
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions })
+
+const cases = [
+  {
+    name: 'Basic',
+    valid: {
+      code: `
+        function Component(props) {
+          return pug\`
+            = props.name
+          \`
+        }
+        Component.propTypes = { name: PropTypes.string }
+      `,
+    },
+    invalid: {
+      code: ``,
+      errors: [],
+    },
+  },
+]
+
+ruleTester.run('rule "eslint"', rule, buildCases(cases))
